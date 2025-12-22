383bc457c549b594e74f5776723e7930dac357c2
ajeyajaz <ajeyajaz@gmail.com>
2025-12-22 14:48:20 +0530

    define the Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Home from './pages/home'
 
 function App() {
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Home/>
     </>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Home(){
+    return(
+        <>
+            <h1>Welcome</h1>
+            <div>
+                <h2>Book categories</h2>
+            </div>
+            <div>
+                <h2> Popular books</h2>
+            </div>
+        </>
+    )
+}
+
+export default Home
