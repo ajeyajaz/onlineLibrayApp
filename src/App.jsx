fdfa3890baee98b6a336493ccb718f53d43c8c42
ajeyajaz <ajeyajaz@gmail.com>
2025-12-23 13:00:20 +0530

    integrate the react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import { useState } from 'react'
 import './App.css'
 import HomePage from './pages/HomePage'
+import BrowseBook from './pages/BrowseBook'
+import AllBooks from './pages/AllBooks'
+import FilterBooks from './components/FilterBooks'
+import { BrowserRouter, Routes, Route } from 'react-router'
 
 function App() {
 
   return (
     <>
-      <HomePage/>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<HomePage />} />
+
+          <Route path='/hii' element={<BrowseBook />}>
+            <Route index element={<AllBooks />} />
+            <Route path='books/:category' element={<FilterBooks />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
